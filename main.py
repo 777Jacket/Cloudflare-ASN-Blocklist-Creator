8e2202d215b41d12fdfaac7780807c693435a602
Klepto <155437891+KleptoFarts@users.noreply.github.com>
2024-01-02 10:28:49 +0000

    Project upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import requests
+
+# Reads the AS Numbers from the text file
+with open('as_numbers.txt', 'r') as file:
+    as_numbers = file.read().splitlines()
+
+# Creates the Cloudflare WAF expression
+expression = ' or '.join([f'(ip.geoip.asnum eq {asn})' for asn in as_numbers])
+
+# Saves the expression to a text file
+with open('expression.txt', 'w') as file:
+    file.write(expression)
+
+print(expression)
